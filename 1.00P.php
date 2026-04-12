201da45f2a962a5e614302c3016728a3951e1750
Gulyás Máté <in@mz.hu>
2026-04-12 12:18:38 +0200

    2 user (életkor + név)

--- 1.00P.php
+++ 1.00P.php
@@ -1,4 +1,7 @@
 <?php
+
+// 1.00P.php    ez a file neve (egy.nulla nulla.php)
+
 class user
 {
     var $név;
@@ -7,8 +10,30 @@ class user
     {
         return date("Y") - $this->birthyear;
     }
-}
-$user=new user;
-$user->birthyear=2000;
-print $user->getAge();
+    function
 
+    getName()
+    {
+        return $this->név;
+    }
+}
+
+
+$user1 = new user;
+$user1->birthyear = 2000;
+print $user1->getAge();
+print " éves jelenleg";
+$user1->név = "Angéla";
+print " ";
+print $user1->getName();
+
+
+print "<br>";
+
+$user2 = new user;
+$user2->birthyear = 1990;
+print $user2->getAge();
+print " éves jelenleg";
+$user2->név = "Béla";
+print " ";
+print $user2->getName();
