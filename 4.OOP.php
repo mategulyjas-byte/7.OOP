d598b74ed06c3fb971f5ff7b7fc3eb35345b69bb
Gulyás Máté <in@mz.hu>
2026-04-14 21:05:24 +0200

    destruct

---
+++ 4.OOP.php
@@ -0,0 +1,25 @@
+<?php
+
+
+class user
+{
+    var $nev, $email;
+
+function __destruct(){
+print " "."destruct";}
+
+    function __construct($name, $mail)
+    {
+        print "construct"." ";
+        $this->nev = $name;
+        $this->email = $mail;
+    }
+function getName(){return $this->nev;}
+function getEmail(){return $this->email;}
+
+}
+
+$user1 = new user ("Pista","emai@email.hu");
+
+print $user1->getName()." email címe:  ".$user1->getEmail();
+
