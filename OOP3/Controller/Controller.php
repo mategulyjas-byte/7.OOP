009013b39324c9749b8256758fa2bc669f300292
Gulyás Máté <in@mz.hu>
2026-04-24 23:02:58 +0200

    MVC

---
+++ OOP3/Controller/Controller.php
@@ -0,0 +1,21 @@
+<?php
+
+class Controller{
+
+function Fooldal()  {include (__DIR__."/../View/Fooldal.php");}
+
+function Elerhetoseh(){
+    include (__DIR__."/../View/Elerhetoseg.php");
+}
+function Galeria(){
+    include (__DIR__."/../View/Galeria.php");}
+ function Gyik(){
+        include (__DIR__."/../View/Gyik.php"); }
+
+function Rolunk(){
+        include (__DIR__."/../View/Rolunk.php");
+
+}
+
+
+}
---
+++ OOP3/index.php
@@ -0,0 +1,28 @@
+<?php
+
+include __DIR__."/Controller/Controller.php";
+
+$url=$_SERVER["REQUEST_URI"];
+
+$oladalak= new Controller;
+
+
+switch ($url){
+
+case "/": $oladalak->Fooldal();
+break;
+
+case '/Rolunk': $oladalak->Rolunk();
+break;
+
+case '/Gyik': $oladalak->Gyik();
+break;
+
+case'/Galeria': $oladalak->Galeria();
+break;
+
+case '/Elerhetoseg': $oladalak->Elerhetoseh();
+break;
+default: print " Hiba!  Kérem ellenőrizze a beírt címet";
+}
+;
