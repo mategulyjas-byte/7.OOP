f4eff1e86043547add857be97f36005fdbcfce22
Gulyás Máté <in@mz.hu>
2026-04-25 15:19:18 +0200

    jav

--- OOP3/index.php
+++ OOP3/index.php
@@ -5,26 +5,19 @@ include __DIR__."/Controller/Controller.php";
 $url=$_SERVER["REQUEST_URI"];
 
 $url=str_replace('/gulyasmate/index.php','',$url);
-
-
+$url=str_replace('/gulyasmate','',$url);
 
 $oladalak= new Controller;
 
-
 switch ($url){
-
 case '/': $oladalak->Fooldal();
 break;
-
 case '/Rolunk': $oladalak->Rolunk();
 break;
-
 case '/Gyik': $oladalak->Gyik();
 break;
-
 case'/Galeria': $oladalak->Galeria();
 break;
-
 case '/Elerhetoseg': $oladalak->Elerhetoseg();
 break;
 default: print " Hiba!  Kérem ellenőrizze a beírt címet";
