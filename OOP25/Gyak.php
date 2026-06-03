d73582f3b00eaa3c2a1cbfc6544107f55b8743de
Gulyás Máté <in@mz.hu>
2026-06-03 21:53:04 +0200

    gyak

--- OOP25/Gyak.php
+++ OOP25/Gyak.php
@@ -1,11 +1,14 @@
 <?php
 
 // insert into users ('name', 'email') VALUES ( 'name', 'email')
+
+use Odbc\Connection;
+
 session_start();
 $connection=mysqli_connect("localhost","root","","gulyas_mate");
 
 
-class Functionok{
+class Function1{
 
 public $connection;
 
@@ -24,12 +27,66 @@ foreach ($Tömb as $tömbelem){
 
 $bevittértékek=rtrim($bevittértékek,",");
 
+ $sql= ("insert into user2 ($tömbelemekmegnevezése) VALUES ($bevittértékek)");
+print_r ($sql);
 
 mysqli_query($this->connection, "insert into user2 ($tömbelemekmegnevezése) VALUES ($bevittértékek)");
+
+
 ;}}
 
+//$functionok1 = new Function1($connection);
+
+//print $functionok1->insert(["name","email","password"]);
 
 
-$functionok = new Functionok($connection);
-print $functionok->insert(["name","email","password"]);
 
+
+
+
+
+
+
+
+ $connection2= mysqli_connect ("localhost","root","","gulyas_mate");
+
+class function2{
+
+public $connection2;
+public $table = "user2";
+
+function __construct($connection)
+{$this->connection2=$connection;
+}
+
+function insert($Tömb2){
+
+//insert into user (name, email,) values ('$_Post[name],$_POST["email]')
+
+
+//implode
+
+$adattáblafejléc= implode(",",$Tömb2);
+$beírandóérték="";
+
+
+foreach( $Tömb2 as $tömbelem){
+
+
+$beírandóérték.="'" .$_POST[$tömbelem]."',";}
+
+
+
+
+$beírandóérték=rtrim($beírandóérték, ",");
+
+//$sqli= ("insert into $this->table ($adattáblafejléc) values ($this->beírandóérték)");
+
+
+mysqli_query($this->connection2,"insert into $this->table ($adattáblafejléc) values ($beírandóérték)");
+
+;}
+}
+$functionok2 = new function2($connection2);
+
+$functionok2->insert(["name", "email","password"]);
---
+++ OOP25/Reg.php
@@ -0,0 +1,27 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+<form action="Gyak.php" method="post"> 
+
+<label for="name" >Name</label>
+<input type="text" name="name" id="name">
+
+<label for="email" >Email</label>
+<input type="text" name="email" id="email">
+
+<label for="password" >Password</label>
+<input type="text" name="password" id="password">
+
+<button type="submit">Regisztáció</button>
+
+</form>
