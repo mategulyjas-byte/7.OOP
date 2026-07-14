e3f27656dca18a87cf214c0491dc2bc3c32781ae
Gulyás Máté <in@mz.hu>
2026-07-14 22:12:06 +0200

    facebookról is megnyitható

--- OOP26_osszefoglalasnetrekitéve/index.php
+++ OOP26_osszefoglalasnetrekitéve/index.php
@@ -37,7 +37,9 @@ $sarkozinfocontroller= new SarkozInfoController;
 
 
 
-$url = $_SERVER["REQUEST_URI"];
+//$url = $_SERVER["REQUEST_URI"];
+
+$url = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
 
 switch ($url) {
     case '/':
