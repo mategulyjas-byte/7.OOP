5edbe044af89fa4ec3fb3826eaf77b3a8c69e99a
Gulyás Máté <in@mz.hu>
2026-08-23 21:42:39 +0200

    sakktábla

---
+++ HTMLCSS2/index.php
@@ -0,0 +1,217 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="css2.css">
+
+    <style>
+        body {
+            box-sizing: border-box;
+        }
+    </style>
+</head>
+
+<body>
+
+
+    <div style="background-color: red;">
+
+        <div style="display:flex; width: 100%; justify-content:space-around    ">
+            <div>a</div>
+            <div>b</div>
+            <div>c</div>
+
+        </div>
+        <div style="background-color: aqua; width: 60px; margin:auto; text-align:center">2</div>
+
+        <div style="display: flex; justify-content:space-between ; width:100%">
+            <div style="background-color: blue; text-align:center ; width:100%"> 3 </div>
+            <div style="background-color:chartreuse;  text-align:center ; width:100%    ">4</div>
+        </div>
+
+    </div>
+    <br>
+    <br>
+    <br>
+    <br>
+
+
+
+    <table style="border-collapse:collapse;  max-width:fit-content; border: 2px solid black; margin:  0 auto;">
+
+        <tr style="border: 2px solid black;">
+            <td style=" background-color:black; padding:20px"></td>
+            <td style=" background-color:white; padding:20px"></td>
+            <td style="background-color:black; padding:20px"></td>
+            <td style="background-color:white; padding:20px"></td>
+            <td style=" background-color:black; padding:20px"></td>
+            <td style="background-color:white; padding:20px"></td>
+            <td style=" background-color:black; padding:20px"></td>
+            <td style=" background-color:white; padding:20px"></td>
+        </tr>
+
+        <tr style=" border: 2px solid black;">
+            <td style=" background-color:white; padding:20px"></td>
+            <td style="background-color:black; padding:20px"></td>
+            <td style="background-color:white; padding:20px"></td>
+            <td style=" background-color:black; padding:20px"></td>
+            <td style="background-color:white; padding:20px"></td>
+            <td style=" background-color:black; padding:20px"></td>
+            <td style=" background-color:white; padding:20px"></td>
+            <td style=" background-color:black; padding:20px"></td>
+
+        </tr>
+
+        <tr style=" border: 2px solid black;">
+            <td style=" background-color:black; padding:20px"></td>
+            <td style=" background-color:white; padding:20px"></td>
+            <td style="background-color:black; padding:20px"></td>
+            <td style="background-color:white; padding:20px"></td>
+            <td style=" background-color:black; padding:20px"></td>
+            <td style="background-color:white; padding:20px"></td>
+            <td style=" background-color:black; padding:20px"></td>
+            <td style=" background-color:white; padding:20px"></td>
+        </tr>
+
+        <tr style="border: 2px solid black;">
+            <td style=" background-color:white; padding:20px"></td>
+            <td style="background-color:black; padding:20px"></td>
+            <td style="background-color:white; padding:20px"></td>
+            <td style=" background-color:black; padding:20px"></td>
+            <td style="background-color:white; padding:20px"></td>
+            <td style=" background-color:black; padding:20px"></td>
+            <td style=" background-color:white; padding:20px"></td>
+            <td style=" background-color:black; padding:20px"></td>
+
+        </tr>
+
+        <tr style="  border: 2px solid black;">
+            <td style=" background-color:black; padding:20px"></td>
+            <td style=" background-color:white; padding:20px"></td>
+            <td style="background-color:black; padding:20px"></td>
+            <td style="background-color:white; padding:20px"></td>
+            <td style=" background-color:black; padding:20px"></td>
+            <td style="background-color:white; padding:20px"></td>
+            <td style=" background-color:black; padding:20px"></td>
+            <td style=" background-color:white; padding:20px"></td>
+        </tr>
+
+        <tr style=" border: 2px solid black;">
+            <td style=" background-color:white; padding:20px"></td>
+            <td style="background-color:black; padding:20px"></td>
+            <td style="background-color:white; padding:20px"></td>
+            <td style=" background-color:black; padding:20px"></td>
+            <td style="background-color:white; padding:20px"></td>
+            <td style=" background-color:black; padding:20px"></td>
+            <td style=" background-color:white; padding:20px"></td>
+            <td style=" background-color:black; padding:20px"></td>
+
+        </tr>
+
+        <tr style="  border: 2px solid black;">
+            <td style=" background-color:black; padding:20px"></td>
+            <td style=" background-color:white; padding:20px"></td>
+            <td style="background-color:black; padding:20px"></td>
+            <td style="background-color:white; padding:20px"></td>
+            <td style=" background-color:black; padding:20px"></td>
+            <td style="background-color:white; padding:20px"></td>
+            <td style=" background-color:black; padding:20px"></td>
+            <td style=" background-color:white; padding:20px"></td>
+        </tr>
+
+        <tr style="  border: 2px solid black;">
+            <td style=" background-color:white; padding:20px"></td>
+            <td style="background-color:black; padding:20px"></td>
+            <td style="background-color:white; padding:20px"></td>
+            <td style=" background-color:black; padding:20px"></td>
+            <td style="background-color:white; padding:20px"></td>
+            <td style=" background-color:black; padding:20px"></td>
+            <td style=" background-color:white; padding:20px"></td>
+            <td style=" background-color:black; padding:20px"></td>
+
+        </tr>
+
+
+
+    </table>
+
+    </div>
+
+
+
+
+
+
+
+
+
+
+    </div>
+
+
+
+
+
+
+
+
+<br><br><br>
+
+
+
+
+
+
+
+
+
+    <table class="table1" style="border-collapse:collapse;  max-width:fit-content; border: 2px solid black; margin:  0 auto;">
+
+        <tr style="border: 2px solid black;">
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+
+        <tr style="border: 2px solid black;">
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+
+
+
+    </table>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+
+</html>
